d9e68994d546c6f5aeeae39bce2bf2ef37f29d5b
Matti <47687174+mattipunkt@users.noreply.github.com>
2023-02-20 11:05:10 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -1,1 +1,15 @@
+import os, sys, sqlite3
 import lib.functions as func
+import logging                      # Zum Loggen
+
+# Logger zur Detaillierten Aufzeichnung, in Vorbereitung auf die einfachere Verwendung des GUIs, vielleicht später py2exe um eine einfache exe-Datei für Windows bereit zustellen.
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+
+# Als erstes überprüfen, ob eine genannte Datenbank bereits existiert
+if os.path.exists("schüler.db"):
+    print("Datei bereits vorhanden")
+    sys.exit(0)
+    
