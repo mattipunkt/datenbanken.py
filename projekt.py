7d5881f1702aa9a643c90a17aa3208ec44e0bad8
Matti <47687174+mattipunkt@users.noreply.github.com>
2023-02-20 11:10:42 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -8,10 +8,17 @@ logging.basicConfig(filename="newfile.log",
                     format='%(asctime)s %(message)s',
                     filemode='w')
 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 # Als erstes überprüfen, ob eine genannte Datenbank bereits existiert
+logger.warning("Versuche Datenbank zu erstellen")
 if os.path.exists("schueler.db"):
     print("Datei bereits vorhanden")
+    logger.warning("Datei konnte nicht erstellt werden, da bereits vorhanden")
     sys.exit(0)
-    
+
+logger.info("Verbinde mit der Datenbank")
 connection = sqlite3.connect("schueler.db")
+logger.info("Erfolg! Verbunden mit der DatenbanK")
