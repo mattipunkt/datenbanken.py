3e8dd0d6427fb68aefe1ab8ba862db71d8a2553b
Matti <47687174+mattipunkt@users.noreply.github.com>
2023-02-21 13:25:55 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -2,9 +2,11 @@
 import os, sys, sqlite3
 import lib.functions as func
 import logging                      # Zum Loggen
+from pathlib import Path            # zum erstellen der Datenbank
+import time
 
 # Logger zur Detaillierten Aufzeichnung, in Vorbereitung auf die einfachere Verwendung des GUIs, vielleicht später py2exe um eine einfache exe-Datei für Windows bereit zustellen.
-logging.basicConfig(filename="newfile.log",
+logging.basicConfig(filename="dblog %s.log" %time.time(),
                     format='%(asctime)s %(message)s',
                     filemode='w')
 
@@ -14,11 +16,31 @@ logger.setLevel(logging.DEBUG)
 
 # Als erstes überprüfen, ob eine genannte Datenbank bereits existiert
 logger.warning("Versuche Datenbank zu erstellen")
-if os.path.exists("schueler.db"):
+if os.path.exists("filme.db"):
     print("Datei bereits vorhanden")
     logger.warning("Datei konnte nicht erstellt werden, da bereits vorhanden")
     sys.exit(0)
 
 logger.info("Verbinde mit der Datenbank")
-connection = sqlite3.connect("schueler.db")
+connection = sqlite3.connect("filme.db")
 logger.info("Erfolg! Verbunden mit der DatenbanK")
+
+def datenbankErstellen():
+    dbname = input("Name der Datenbank: ")
+    print("Der Datenbankname wurde festgelegt auf: ",dbname)
+    return
+
+
+def tabelleErstellen();
+    sql = "CREATE TABLE filme(" \
+      "titel TEXT, " \
+      "vorname TEXT, " \
+      "personalnummer INTEGER PRIMARY KEY, " \
+      "gehalt REAL, " \
+      "geburtstag TEXT)"
+
+print("Wilkommen in deiner persönlichen Filmdatenbank.")
+print("Folgende Eingaben sind möglich: ")
+print("add      -       Hinzufügen eines Films")
+
+print(time.time())
