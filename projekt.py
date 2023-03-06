e2e6802e701259ca5fb2a572229091881a19da2f
Matti Weidlich <matti.hro@systemausfall.org>
2023-03-06 10:23:12 +0100

    added menu control

--- projekt.py
+++ projekt.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-import os, sys, sqlite3
-import lib.functions as func
+import os, sqlite3
+
 import logging                      # Zum Loggen
 from pathlib import Path            # zum erstellen der Datenbank
 import time
@@ -14,8 +14,6 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
-
-
 def pushtodb():
     connection = sqlite3.connect("filme.db")  # Verbinde mit der Datenbank
     connection.commit()
@@ -27,14 +25,11 @@ def datenbankerstellen():
     return
 
 def testtable():
-    cursor = connection.cursor
     print('Tabellentest wird gestartet.')
     test = 'SELECT * from filme;'
     cursor.execute(test)
     print('Hier der aktuelle Stand der Datenbank')
-    for dsatz in cursor:
-        print(dsatz[0], dsatz[1], dsatz[2],
-          dsatz[3], dsatz[4])
+
     return
 
 
@@ -60,7 +55,12 @@ def addmovie(title, cast, length, director, rating, streaming, genre):
     connection.commit()
     connection.close()
     print("Film {} erfolgreich hinzugefügt!".format(title))
-    return
+    print("Kehre zurück zum Hauptmenü!")
+    main()
+
+
+def deletemovie():
+    sql = ''
 
 # Als erstes überprüfen, ob eine genannte Datenbank bereits existiert
 logger.warning("Versuche Datenbank zu erstellen")
@@ -71,6 +71,33 @@ else:
     createtable()
 
 
+def main():
+    print("Aktionsmöglichkeiten:")
+    print("a - Film hinzufügen,   d - Film löschen,    l - Filme anzeigen,     s - Film suchen")
+    selection = input("Aktion wählen: ")
+    if selection == "a":
+        print("Film hinzufügen!")
+        filmip = input("Name des Filmes: ")
+        actorip = input("Schauspieler: ")
+        laengeip = input("Länge: ")
+        regisseurip = input("Regisseur: ")
+        bewertungip = input("Bewertung (0 bis 10 inkl. Nachkommastellen: ")
+        streamingverfeugbarip = input("Verfügbar bei: ")
+        genreip = input("Genre: ")
+        addmovie(filmip, actorip, laengeip, regisseurip, bewertungip, streamingverfeugbarip, genreip)
+    elif selection == "d":
+        print("Film löschen!")
+        print("TBC")
+    elif selection == "l":
+        print("Filme anzeigen!")
+        print("tbc")
+    elif selection == "s":
+        print("Film suchen!")
+    else:
+        print("Fehler! Falsche Eingabe.")
+        main()
+
+
 logger.info("Verbinde mit der Datenbank")
 connection = sqlite3.connect("filme.db")  # Verbinde mit der Datenbank
 logger.info("Erfolg! Verbunden mit der DatenbanK")
@@ -78,14 +105,8 @@ print("Verbunden mit der Datenbank")
 print("SQLite3 Version: " + sqlite3.version)
 cursor = connection.cursor()  # Zum erstellen von Sachen
 
-print("Film hinzufügen!")
-filmip = input("Name des Filmes: ")
-actorip = input("Schauspieler: ")
-laengeip = input("Länge: ")
-regisseurip = input("Regisseur: ")
-bewertungip = input("Bewertung (0 bis 10 inkl. Nachkommastellen: ")
-streamingverfeugbarip = input("Verfügbar bei: ")
-genreip = input("Genre: ")
-addmovie(filmip, actorip, laengeip, regisseurip, bewertungip, streamingverfeugbarip, genreip)
+print('time: {}')
+print('Wilkommen in der Film-Datenbank!')
+main()
 
-print(time.time())
+
